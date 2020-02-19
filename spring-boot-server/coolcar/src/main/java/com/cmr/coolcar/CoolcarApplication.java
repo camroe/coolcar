e29ebf4e6932b10db87344d601ab776587c4d256
Cam Roe <croe10@ford.com>
2020-02-19 14:57:10 -0500

    Initial Spring Boot Application and root directory

---
+++ spring-boot-server/coolcar/src/main/java/com/cmr/coolcar/CoolcarApplication.java
@@ -0,0 +1,13 @@
+package com.cmr.coolcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolcarApplication.class, args);
+	}
+
+}
---
+++ spring-boot-server/coolcar/src/test/java/com/cmr/coolcar/CoolcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmr.coolcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
